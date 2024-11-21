a0abb4f32bb8f9af37b5acb8d212513e299dc4b3
Crhistian Araujo <113248738+crhisdotaraujo@users.noreply.github.com>
2024-11-21 16:02:51 +0000

    Adds database `health status` information

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  let databaseQuery;
+
+  const updatedAt = new Date().toISOString();
+  databaseQuery = await database.query("SHOW server_version;");
+  const databaseVersion = databaseQuery.rows[0].server_version;
+  databaseQuery = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = databaseQuery.rows[0].max_connections;
+  databaseQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databaseOpennedConnections = databaseQuery.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        openned_connections: databaseOpennedConnections,
+      },
+    },
+  });
 }
 
 export default status;
