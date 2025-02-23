860e4c6e803adfe222f33d6b3da5ef8a5f87258d
Crhistian Araujo <113248738+crhisdotaraujo@users.noreply.github.com>
2025-02-23 11:48:11 +0000

    Makes `npm run test` script more reliable with `async-retry` and `orchestrator`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("GET to api/v1/status should be 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
